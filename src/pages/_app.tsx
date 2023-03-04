66480d45bf2ff90f6a01f8f6c0a1a2c177609870
Chris Ren <ren.chris@outlook.com>
2023-03-03 22:21:53 -0700

    feat: ThirdWeb Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import { ThirdwebProvider } from '@thirdweb-dev/react'
+import { MoonbaseAlpha } from '@thirdweb-dev/chains'
+
 import theme from '../../theme'
+import Container from '../components/Container'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider resetCSS theme={theme}>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  <ThirdwebProvider activeChain={MoonbaseAlpha}>
+    <ChakraProvider resetCSS theme={theme}>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  </ThirdwebProvider>
 )
-
 export default MyApp
