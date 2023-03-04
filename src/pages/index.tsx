9d4d3391f8cdb3ad1f9ba2a38f27a70dff73a55c
Chris Ren <ren.chris@outlook.com>
2023-03-03 21:33:21 -0700

    feat: remove nav bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,14 @@ import {
   ListItem,
 } from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-
 import Hero from '../components/Hero'
 import Container from '../components/Container'
 import Main from '../components/Main'
-import DarkModeSwitch from '../components/DarkModeSwitch'
 import CTA from '../components/CTA'
 import Footer from '../components/Footer'
-import NavigationBar from '../components/NavigationBar'
 
 const Index = () => (
   <Container height="100vh">
-    <NavigationBar />
     <Hero title="with-chakra-ui-typescript" />
     <Main>
       <Text color="text">
