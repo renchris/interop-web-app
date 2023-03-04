749e0ead72c3cab1b52f8d525b5e1e98d7773a81
Chris Ren <ren.chris@outlook.com>
2023-03-04 00:09:05 -0700

    feat: interop logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { ThirdwebProvider } from '@thirdweb-dev/react'
 import { MoonbaseAlpha } from '@thirdweb-dev/chains'
-
+import Head from 'next/head'
 import theme from '../../theme'
 import Container from '../components/Container'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThirdwebProvider activeChain={MoonbaseAlpha}>
     <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+        <link rel="icon" type="image/png" href="favicon.png" />
+      </Head>
       <Container>
         <Component {...pageProps} />
       </Container>
